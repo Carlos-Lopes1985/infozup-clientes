12248d9fdb0ff7fd8286001b2bbfefe79199dd5b
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2022-09-07 00:40:18 -0300

    primeiro commit

---
+++ src/main/java/com/infozup/cadastrocliente/PocCadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.infozup.cadastrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCadastroClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infozup/cadastrocliente/PocCadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infozup.cadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocCadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
